bafe71c800dc2a94cce7d802facefdbea1bf16ff
sofiagiraldo01 <sgiraldo_34@cue.edu.co>
2025-04-03 08:30:06 -0500

    actualizacion7

--- src/main/java/org/arle/proyectorolesauth/config/SecurityConfig.java
+++ src/main/java/org/arle/proyectorolesauth/config/SecurityConfig.java
@@ -37,10 +37,7 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+   
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
